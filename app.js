dd5e33d2991b352f5473d9c4e4263c796d929c22
Kirstin Reid <kirstinreid@kirstins-mbp.home>
2017-02-06 21:19:01 +0000

    create app server in node js with express

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /**
  * Created by kirstinreid on 02/02/2017.
  */
+/**
+
 var express = require("express");
 
 /**
@@ -13,7 +15,7 @@ require('./positions.js');
 require('./newuser.js');
 require('./mainuser.js');
 require('./callback.html');
-**/
+
 var app = express();
 
 http = require('http');
@@ -25,3 +27,53 @@ app.get('/index.html', function (req, res) {
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
+*/
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+}).listen(process.env.PORT || 5000);
+
+console.log('Server running at http://127.0.0.1:5000/');
